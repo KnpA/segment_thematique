6b2d77dd39e5f992714c70032ed98886420cbcf6
Morgan Bouculat <morgan.bouculat@gmail.com>
2016-01-19 14:53:50 +0100

    Amélioration de la fonction Tokenize

--- normalisation.py
+++ normalisation.py
@@ -1,7 +1,8 @@
+# coding: utf8
 import sys,glob,subprocess,shlex
 
 reload(sys)  
-sys.setdefaultencoding('iso-8859-1')
+sys.setdefaultencoding('utf-8')
 
 def Test():
     #Fichiers texte
@@ -12,16 +13,14 @@ def ReadTextFiles():
     for textfile in glob.iglob('.\donnees\Ecrits\*.txt'):
         filename = textfile.split("\\")
         filename = filename[0]+"/"+filename[1]+"/"+filename[2]+"/"+filename[3]
-        print filename
-        Tokenize(filename)
+        text = Tokenize(filename)
+        print "File: "+filename+"\n"+text
 
 def Tokenize(filename):
-    print "Tokenize "+filename
-    args = shlex.split("perl ./normalization/tokenizer.pl --no-line-seg --datafile ./normalization/tokenizer.data --language FRENCH < "+filename)
-    print args
+    args = shlex.split("perl ./normalization/tokenizer.pl --no-line-seg --datafile ./normalization/tokenizer.data --language FRENCH "+filename)   
     p = subprocess.Popen(args, stdout=subprocess.PIPE)
-    pipetext = p.communicate()
-    print str(pipetext[0])
+    pipetext = p.communicate()[0]
+    return pipetext
 
 if __name__ == '__main__':
     Test()
