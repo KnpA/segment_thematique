43a3c7a30796ce8ed151bc6eec67622b20f85761
Adrien Dourthe <adrien.dourthe@gmail.com>
2016-01-25 14:24:40 +0100

    Segmentation chaines v1

--- segmentation_chaines.py
+++ segmentation_chaines.py
@@ -17,11 +17,15 @@ stub = [
 def Test():
     print "Segmentation chaines OK"
     
-def Segmentation(phrases, tailleChaine) :
-    segments = []
+def Segmentation(phrases, tailleChaine, seuil) :
     
     chaines = CreerChaines(phrases, tailleChaine)
-     
+    
+    print chaines
+    
+    segments = CreerSegments(phrases, chaines, seuil)
+    
+    print segments
     return segments
 
 def CreerChaines(phrases, tailleChaine) :
@@ -36,14 +40,15 @@ def CreerChaines(phrases, tailleChaine) :
             
             # MAJ chaines
             if mot in compteurs.keys() :
-                print chaines[mot]
-                chaines[mot]
+                
+                chaines[mot][-1].append(i)
                 
             else :
                 if mot not in chaines.keys() :
                     chaines[mot] = []
                     
-                chaines[mot].append([])
+                chaines[mot].append([i])
+                
             
             # MAJ compteurs
             for cleCompteur in compteurs.keys() :
@@ -54,7 +59,6 @@ def CreerChaines(phrases, tailleChaine) :
                     compteurs[cleCompteur] = compteurs[cleCompteur]-1
                 
             compteurs[mot] = tailleChaine
-            print compteurs
             
             i += 1;
             
@@ -62,16 +66,49 @@ def CreerChaines(phrases, tailleChaine) :
     return chaines
 
 
-def CreerSegments(chaines, tailleChaine) :
+def CreerSegments(phrases, chaines, seuil) :
     segments = []
     
+    segment = []
+    
+    i = -1
+    for phrase in phrases :
+        i += len(phrase)
+        
+        segment += phrase
+        
+        print nbChaines(chaines, i)
+        
+        if (nbChaines(chaines, i) <= seuil) :
+            
+            print "segment"
+            
+            segments.append(segment)
+            
+            segment = []
+    
     return segments
+
+def nbChaines(chaines, position) :
+    
+    cpt = 0;
+    
+    for key in chaines.keys() :
+        
+        #print chaines[key]
+        for chaine in chaines[key] :
+            #print chaine
+            if min(chaine) <= position and max(chaine) >= position :
+                cpt += 1
+        
+        
+    return cpt
             
             
 
 def Main():
     #print stub
-    Segmentation(stub, 3)
+    Segmentation(stub, 4, 2)
 
 if __name__ == '__main__':
     Main()
