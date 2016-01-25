102c7433c337cfbf956b5a12c0b2c951687cbc37
Morgan Bouculat <morgan.bouculat@gmail.com>
2016-01-25 12:10:22 +0100

    Normalisation des fichiers textes DONE

--- normalisation.py
+++ normalisation.py
@@ -5,28 +5,44 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def Test():
-    #Fichiers texte
-    ReadTextFiles()
-    print "Normalisation OK"
-        
-def ReadTextFiles():
+    #Lecture des textes et normalisation
+    print "Normalisation des textes..."
     for textfile in glob.iglob('.\donnees\Ecrits\*.txt'):
-        filename = textfile.split("\\")
-        filename = filename[0]+"/"+filename[1]+"/"+filename[2]+"/"+filename[3]
-        print "File: "+filename        
-        text = Tokenize(filename)
-        print text
+        array = ReadTextFile(textfile)
+    print "Normalisation OK"
+
+def ReadTextFile(filepath):
+    filepath = filepath.split("\\")
+    filename = filepath[3]
+    filepath = filepath[0]+"/"+filepath[1]+"/"+filepath[2]+"/"+filepath[3]
+    print "Reading file "+filename
+    return Tokenize(filepath)
+        
 
 def Tokenize(filename):
-    args = shlex.split("perl normalization/tokenizer.pl --no-line-seg --datafile normalization/tokenizer.data --language FRENCH "+filename)   
+    #Tokenize
+    args = shlex.split("perl normalization/tokenizer.pl --datafile normalization/tokenizer.data --language FRENCH "+filename)   
     p = subprocess.Popen(args, stdout=subprocess.PIPE)
     pipetext = p.communicate()[0]
-    print pipetext
-    args = shlex.split("perl normalization/normalizer.pl --no-line-seg FRENCH "+filename)   
+
+    #Copy text to temp file
+    f = open('tmp', 'w')
+    f.write(pipetext)
+    f.close()
+
+    #Normalize
+    args = shlex.split("perl normalization/normalizer.pl FRENCH tmp")   
     p = subprocess.Popen(args, stdout=subprocess.PIPE)
     pipetext = p.communicate()[0]
-    print pipetext
-    return pipetext
+
+    #Convert to array
+    sentences = pipetext.split("\n")
+    array = []
+    for sentence in sentences:
+        words = sentence.split( )
+        array.append(words)
+
+    return array
 
 if __name__ == '__main__':
     Test()
