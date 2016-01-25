ce924f718f7dd8d076401de8f666f72b3f95b887
Adrien Dourthe <adrien.dourthe@gmail.com>
2016-01-25 12:10:25 +0100

    compteurs ok

--- segmentation_chaines.py
+++ segmentation_chaines.py
@@ -17,13 +17,54 @@ stub = [
 def Test():
     print "Segmentation chaines OK"
     
-def Segmentation(phrases):
+def Segmentation(phrases, tailleChaine) :
     segments = []
+    
+    chaines = CreerChaines(phrases, tailleChaine)
+     
     return segments
+
+def CreerChaines(phrases, tailleChaine) :
+    
+    chaines = {}
+    compteurs = {}
+    
+    i = 0
+    for phrase in phrases :
+        for mot in phrase :
+            
+            if mot in compteurs.keys() :
+                
+            
+            # MAJ compteurs
+            for cleCompteur in compteurs.keys() :
+                
+                #chaines[mot] = 
+                
+                if compteurs[cleCompteur] == 1 :
+                    del compteurs[cleCompteur]
+                else : 
+                    compteurs[cleCompteur] = compteurs[cleCompteur]-1
+                
+            compteurs[mot] = tailleChaine
+            print compteurs
+            
+            i += 1;
+            
+            
+    return chaines
+
+
+def CreerSegments(chaines, tailleChaine) :
+    segments = []
+    
+    return segments
+            
+            
 
 def Main():
     #print stub
-    Segmentation(stub)
+    Segmentation(stub, 3)
 
 if __name__ == '__main__':
     Main()
