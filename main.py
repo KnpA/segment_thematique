a7ef5b311a1965f2906718264793d770ef6c3eeb
DR <drouzaud.enssat@gmail.com>
2016-02-02 08:47:45 +0100

    xml

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def Main():
     print "Main"
 
 if __name__ == '__main__':
+    normalisation.ReadXMLFiles()
     normalisation.Test()
     segmentation.Test()
     structuration.Test()
--- normalisation.py
+++ normalisation.py
@@ -1,8 +1,30 @@
 # coding: utf8
 import sys,glob,subprocess,shlex
+from lxml import etree
 
 reload(sys)  
 sys.setdefaultencoding('utf-8')
+
+def ReadXMLFiles():
+    for textfile in glob.iglob('./donnees/Transcriptions/ES/transcriptions/automatique/*.ssd'):
+        filename = textfile#.split("\\");
+        #filename = filename[1];
+        print "________________________________________________________________________________";
+        tree = etree.parse(filename)
+        for ts in tree.xpath("/ssdoc/transcript/ts"):
+            spk = ts.get("spk")
+            start = ts.get("start")
+            print ts.get("spk")
+            text = "";
+            for w in tree.xpath("/ssdoc/transcript/ts/w"):
+                #print(" pop ")
+                #w.getparent().get("spk")==spk and
+                if ( w.getparent().get("start")==start):
+                    text = text + w.get("str")+ " "
+                    print "passe"
+            
+            print("text : "+text)
+    
 
 def Test():
     #Fichiers texte
@@ -13,19 +35,19 @@ def ReadTextFiles():
     for textfile in glob.iglob('.\donnees\Ecrits\*.txt'):
         filename = textfile.split("\\")
         filename = filename[0]+"/"+filename[1]+"/"+filename[2]+"/"+filename[3]
-        print "File: "+filename        
+        #print "File: "+filename        
         text = Tokenize(filename)
-        print text
+        #print text
 
 def Tokenize(filename):
     args = shlex.split("perl normalization/tokenizer.pl --no-line-seg --datafile normalization/tokenizer.data --language FRENCH "+filename)   
     p = subprocess.Popen(args, stdout=subprocess.PIPE)
     pipetext = p.communicate()[0]
-    print pipetext
+    #print pipetext
     args = shlex.split("perl normalization/normalizer.pl --no-line-seg FRENCH "+filename)   
     p = subprocess.Popen(args, stdout=subprocess.PIPE)
     pipetext = p.communicate()[0]
-    print pipetext
+    #print pipetext
     return pipetext
 
 if __name__ == '__main__':
